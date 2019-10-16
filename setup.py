600aa84339c65856a473be34f9f2616319d928ec
Yonatan Striem-Amit <yonatan@cybereason.com>
2019-10-16 10:28:49 -0400

    Adding requests to requirements

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['six', 'colorama', 'psutil'],  # Optional
+    install_requires=['six', 'colorama', 'psutil', 'requests'],  # Optional
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
